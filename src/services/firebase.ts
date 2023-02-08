8428edd687b3a758d4148667e653adbe22792806
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2023-02-08 17:45:31 -0300

    Inicio do projeto e integração do firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getDatabase } from "firebase/database";
+import "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MENSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const database = firebase.database();
+
+// test firebase
+// try {
+//   const app = initializeApp(firebaseConfig);
+//   console.log('Firebase conectado com sucesso!');
+// } catch (error) {
+//   console.error('Erro ao conectar ao Firebase: ', error);
+// }
